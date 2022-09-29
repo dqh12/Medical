ca8d3fe458110720faa767c838a620b38997f6e4
dqh12 <834918501@qq.com>
2022-09-29 16:53:46 +0800

    Expection handler

--- common/common_util/src/main/java/com/henu/common/util/GlobalExceptionHandler.java
+++ common/common_util/src/main/java/com/henu/common/util/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package com.henu.common.util;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ControllerAdvice: 表示当前类是异常处理类，给Controller控制器类增强功能的
+ */
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * 定义处理异常的方法，当异常发生后，执行这个方法
+     * 处理Exception异常
+     *
+     * @ExceptionHandler: 表示此方法处理相应的异常
+     *               属性: value 表示异常的类型
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+    /**
+     * 定义处理异常的方法，当异常发生后，执行这个方法
+     * 处理SumException异常
+     *
+     * @ExceptionHandler: 表示此方法处理相应的异常
+     *               属性: value 表示异常的类型
+     */
+    @ExceptionHandler(SumException.class)
+    @ResponseBody
+    public Result error(SumException e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
 }
--- common/common_util/src/main/java/com/henu/common/util/SumException.java
+++ common/common_util/src/main/java/com/henu/common/util/SumException.java
@@ -1,4 +1,35 @@
 package com.henu.common.util;
 
-public class SumException {
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+//自定义全局异常类
+@Data//lombok
+@ApiModel("自定义全局异常类")
+public class SumException extends RuntimeException{
+
+    @ApiModelProperty(value = "异常状态码")
+    private Integer code;
+
+    //通过状态码和错误消息创建异常对象
+    public SumException(String message, Integer code) {
+        super(message);
+        this.code = code;
+    }
+
+    //接收枚举类型对象
+
+    public SumException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "YyghException{" +
+                "code=" + code +
+                ", message=" + this.getMessage() +
+                '}';
+    }
 }
